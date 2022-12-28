76867548dd1e62fff0e771ef0a5d8b66ca1b746f
Wilson-Emmanuel <wilsonemmanuel144@gmail.com>
2022-12-29 03:15:40 +0900

    implemented tests

---
+++ src/test/java/com/anymind/paymentservice/PaymentMethodTest.java
@@ -0,0 +1,88 @@
+package com.anymind.paymentservice;
+
+import com.anymind.paymentservice.web.models.requests.PaymentInput;
+import com.anymind.paymentservice.web.models.requests.PaymentMethodInput;
+import com.anymind.paymentservice.web.models.responses.Payment;
+import com.anymind.paymentservice.web.models.responses.PaymentMethod;
+import com.anymind.paymentservice.web.models.responses.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by Wilson
+ * On 28-12-2022, 21:00
+ */
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PaymentMethodTest {
+
+    @Autowired
+    private  WebApplicationContext applicationContext;
+
+    private HttpGraphQlTester tester;
+
+    private PaymentMethod expectedPaymentMethod;
+
+    @BeforeAll
+    public  void setup(){
+
+        this.expectedPaymentMethod = new PaymentMethod(
+                "UnionPay",
+                new BigDecimal("0.95"),
+                new BigDecimal("1.0"),
+                new BigDecimal("0.05"),
+                true
+        );
+
+        WebTestClient client =
+                MockMvcWebTestClient.bindToApplicationContext(applicationContext)
+                        .configureClient()
+                        .baseUrl("/graphql")
+                        .build();
+
+        tester = HttpGraphQlTester.create(client);
+    }
+
+    @Test
+    void onStartup_load_12_paymentMethods_to_DB_then_successful(){
+        assertTrue(getPaymentMethodCount() >= 12);
+    }
+
+    @Test
+    void add_new_paymentMethod_then_successful(){
+
+        int beforeAdding = getPaymentMethodCount();
+
+        PaymentMethod addedPaymentMethod = this.tester.documentName("addNewPaymentMethod")
+                .execute()
+                .path("createPaymentMethod")
+                .entity(PaymentMethod.class)
+                .get();
+
+        int afterAdding = getPaymentMethodCount();
+
+        assertEquals(beforeAdding+1, afterAdding);
+        assertEquals(expectedPaymentMethod, addedPaymentMethod);
+    }
+    private int getPaymentMethodCount(){
+       return this.tester.documentName("allPaymentMethods")
+                .execute()
+                .path("getPaymentMethods")
+                .entityList(Object.class)
+                .get().size();
+    }
+}
---
+++ src/test/java/com/anymind/paymentservice/PaymentTest.java
@@ -0,0 +1,93 @@
+package com.anymind.paymentservice;
+
+import com.anymind.paymentservice.web.models.requests.PaymentInput;
+import com.anymind.paymentservice.web.models.responses.Payment;
+import com.anymind.paymentservice.web.models.responses.PaymentMethod;
+import com.anymind.paymentservice.web.models.responses.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by Wilson
+ * On 28-12-2022, 21:00
+ */
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PaymentTest {
+
+    @Autowired
+    private  WebApplicationContext applicationContext;
+
+    private HttpGraphQlTester tester;
+
+
+    @BeforeAll
+    public  void setup(){
+        WebTestClient client =
+                MockMvcWebTestClient.bindToApplicationContext(applicationContext)
+                        .configureClient()
+                        .baseUrl("/graphql")
+                        .build();
+
+        tester = HttpGraphQlTester.create(client);
+    }
+
+    @Test
+    void when_makePayment_then_successful(){
+
+        int sizeBeforeMakingPayment = this.tester.documentName("queryPayments")
+                .execute()
+                .path("getPayments")
+                .entityList(Object.class)
+                .get().size();
+
+        //then make a payment with master card
+        Payment paymentResponse = this.tester.documentName("makePaymentWithMastercard")
+                .execute()
+                .path("makePayment")
+                .entity(Payment.class)
+                .get();
+
+        //now get the new count after the test
+        int sizeAfterMakingPayment = this.tester.documentName("queryPayments")
+                .execute()
+                .path("getPayments")
+                .entityList(Object.class)
+                .get().size();
+
+        //get master card payment details
+        PaymentMethod masterCard = this.tester.documentName("getMastercardPaymentMethod")
+                .execute()
+                        .path("getPaymentMethod")
+                                .entity(PaymentMethod.class)
+                                        .get();
+
+        //Confirm that size increased by 1
+        //All confirm that the returned payment is indeed what was sent
+        assertEquals(sizeAfterMakingPayment, sizeBeforeMakingPayment+1);
+        assertEquals(masterCard.getName(), paymentResponse.getPaymentMethod());
+
+
+        //confirm the final price and points are well calculated
+        BigDecimal requestPrice = new BigDecimal("800.00");
+        BigDecimal requestPriceModifier = new BigDecimal("0.95");
+        BigDecimal responseFinalPrice = new BigDecimal(paymentResponse.getFinalPrice());
+
+        assertEquals(responseFinalPrice, requestPriceModifier.multiply(requestPrice).setScale(2,RoundingMode.HALF_EVEN));
+        assertEquals(paymentResponse.getPoints(), requestPrice.multiply(masterCard.getPointModifier()).intValue());
+
+    }
+}
